47858dcd5acffce4b84c822cb7a0f99437e1f1b5
cibulskyte <cibulskytte@gmail.com>
2023-09-13 21:55:05 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var vardas = 'John'
+let pavarde = 'Doe'
+const amzius = '25m'
+const asmensKodas = '496382475648'
+var gyvenamasisMiestas ='Kaunas'
+
+console.log(vardas)
+console.log(pavarde)
+console.log(amzius)
+console.log(asmensKodas)
+console.log(gyvenamasisMiestas)
+
+
+
+console.log(vardas + ' ' + pavarde + ' ' + ' '+ '(Amzius' + ' ' + amzius + ') ' + 'Asmens kodas:' + ' ' + asmensKodas + ' ' + 'Gyvenamasis miestas:' +  ' ' + gyvenamasisMiestas)
+console.log(vardas + " " + pavarde + " " + "(Amzius"+ " " + amzius + ") " + "Asmens kodas" + " " + asmensKodas+ " "+ "Gyvenamasis miestas" + " " + gyvenamasisMiestas)
+console.log(`${vardas} ${pavarde} (Amzius ${amzius}) Asmens kodas: ${asmensKodas} Gyvenamasis miestas: ${gyvenamasisMiestas}`)
